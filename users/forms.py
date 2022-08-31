eaab4e8a7763589a87f0dad6777160a6df56f5f7
Diego Duarte <diemanuel58@gmail.com>
2022-08-31 19:09:26 -0400

    fix: query before startup

--- users/forms.py
+++ users/forms.py
@@ -4,15 +4,7 @@ import users.models
 
 
 class FormRoles(forms.Form):
-    #tomamos todos los permisos de la base de datos
-    permissions_db=users.models.permission.objects.all()
-    CHOICES=[]
-
-    for perm in permissions_db:
-        #agregamos el nombre y el id de cada permiso en una lista que usaremos para el checklist
-        CHOICES.append((perm.id,perm.name))
-        
     name_role=forms.CharField(max_length=100) #nombre del rol
     description_role=forms.CharField( max_length=100) #descripcion del rol
     #sera de multiples opciones con un checkbox, las opciones son los permisos
-    permissions=forms.MultipleChoiceField(choices=CHOICES, widget=forms.CheckboxSelectMultiple())
+    permissions=forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple())
--- users/views.py
+++ users/views.py
@@ -12,9 +12,11 @@ def agg_roles(request):
 
     if request.method=="POST": 
         formRolPost=FormRoles(request.POST) #creamos un form con los datos cargados
-
+        
         if formRolPost.is_valid(): #vemos si es valido
             formRolNew=formRolPost.cleaned_data #tomamos los datos
+            print ("form enviado")
+            print(formRolNew)
             oldRole=users.models.role.objects.all().filter(name=formRolNew['name_role']) #vemos si hay un rol en la bd con ese nombre
             if (len(oldRole)==0):
                 newRole=users.models.role(name=formRolNew['name_role'],description=formRolNew['description_role']) #creamos un rol
@@ -30,6 +32,17 @@ def agg_roles(request):
             else:
                 isRoleSave=2 #el rol ya existe
     #form vacio para el template
+    #tomamos todos los permisos de la base de datos
+    permissions_db=users.models.permission.objects.all()
+    
+    CHOICES=[]
+
+    for perm in permissions_db:
+        #agregamos el nombre y el id de cada permiso en una lista que usaremos para el checklist
+        CHOICES.append((perm.id,perm.name))
+
+    print (CHOICES)
     formRol = FormRoles()
+    formRol.fields['permissions'].choices=CHOICES #agregamos los permisos al form
     #enviamos el form vacio y el numero que indica si se cargo un rol o no
     return render(request, 'agg_roles.html', {'formRol': formRol,'isRoleSave':isRoleSave}) 
