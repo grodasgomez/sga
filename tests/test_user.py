20061ff780f56f8ce2f1eaaf602e582772132a67
Curda27 <carlosurdapilleta@gmail.com>
2022-09-07 21:12:00 -0400

    test: added tests for user model

--- tests/test_user.py
+++ tests/test_user.py
@@ -4,11 +4,23 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sga.settings")
 setup()
 
+from users.models import CustomUser
 
 class TestUser(TestCase):
-    def test_prueba1(self):
-        self.assertEqual(5 + 3, 8)
+    """
+    Clase para probar el modelo de usuario
+    """
+    def test_is_not_user(self):
+        user = CustomUser()
+        user.role_system = ""
+        self.assertNotEqual(user.is_user(), True, "User is user")
 
-    def test_prueba2(self):
-        self.assertEqual(5 - 3, 2)
+    def test_is_user(self):
+        user = CustomUser()
+        user.role_system = "user"
+        self.assertEqual(user.is_user(), True, "User is not user")
 
+    def test_is_admin(self):
+        user = CustomUser()
+        user.role_system = "admin"
+        self.assertEqual(user.is_admin(), True, "User is not admin")
