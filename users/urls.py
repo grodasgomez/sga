446a9327425bf1a3f0a5aa522412368b2c7ced9c
Curda27 <carlosurdapilleta@gmail.com>
2022-09-09 02:07:22 -0400

    refactor: removed stuff

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,5 @@
-
-from django.contrib import admin
 from django.urls import path
-from django.urls import include
 from . import views
-from users import views as views_users
 
 urlpatterns = [
     path('', views.UsersView.as_view(), name='index'),
