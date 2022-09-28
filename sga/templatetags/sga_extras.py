5a1e50107cfc6cf5d8709ddef35ba395100ca7e2
Iugdor <grodasgomez@gmail.com>
2022-09-27 22:27:15 -0400

    feat: initial breadcrumb

--- sga/templatetags/sga_extras.py
+++ sga/templatetags/sga_extras.py
@@ -27,3 +27,35 @@ def get_path_param(request, arg_name):
     if arg_name in kwargs:
         return kwargs[arg_name]
     return None
+
+@register.simple_tag(takes_context=True)
+def breadcrumb(context):
+    """
+    Retorna una lista de tuplas (nombre, url) para el breadcrumb
+
+    Ejemplo
+    {% breadcrumb 'Proyectos' 'projects:list' 'Roles' 'projects:roles' %}
+    """
+    request = context['request']
+    path = request.path
+
+    breadcrumb = []
+    while path:
+
+        match = re.search(r'(?P<model_name>[\w-]+)/(?P<model_id>[\w-]+)/?', path)
+        print(match)
+        if not match:
+            break
+        path = path[match.end():]
+        model_name = match.group('model_name')
+        model_id = match.group('model_id')
+        model_name_id = f'{model_name}-{model_id}'
+        model_url = f'{model_name}/{model_id}'
+        previous_path = breadcrumb[-1][1] if breadcrumb else ''
+
+        breadcrumb.append((model_name, f"{previous_path}/{model_name}"))
+        breadcrumb.append((model_name_id, f"{previous_path}/{model_url}"))
+    if(path):
+        previous_path = breadcrumb[-1][1] if breadcrumb else ''
+        breadcrumb.append((path, f'{previous_path}/{path}'))
+    return " | ".join([f'<a href="{url}">{name}</a>' for name, url in breadcrumb])
