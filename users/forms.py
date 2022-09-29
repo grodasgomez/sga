ea2a9c9f48b0ba88eed74139e0ca44300d07e2b9
Curda27 <carlosurdapilleta@gmail.com>
2022-09-29 03:36:53 -0400

    fix: profile edit form

--- users/forms.py
+++ users/forms.py
@@ -2,16 +2,14 @@ from django import forms
 
 from users.models import CustomUser
 from sga import widgets
+
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = CustomUser
-        exclude = ('password', 'is_active', 'last_login', 'deleted_at')
+        exclude = ('password', 'is_active', 'last_login', 'deleted_at', 'sprints')
         widgets = {
             'first_name': widgets.TextInput(),
             'last_name': widgets.TextInput(),
             'email': widgets.TextInput(attrs={'readonly': True}),
             'role_system': widgets.TextInput(attrs={'readonly': True}),
         }
-
-
-
