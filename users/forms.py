bbfcdb4fdc0bbdd28656bbff19cc1ad42c60a98f
Curda27 <carlosurdapilleta@gmail.com>
2022-10-15 15:38:58 -0300

    fix: update profile user without system role

--- users/forms.py
+++ users/forms.py
@@ -4,6 +4,10 @@ from users.models import CustomUser
 from sga import widgets
 
 class ProfileForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['role_system'].required = False
+
     class Meta:
         model = CustomUser
         exclude = ('password', 'is_active', 'last_login', 'deleted_at', 'sprints')
@@ -11,5 +15,5 @@ class ProfileForm(forms.ModelForm):
             'first_name': widgets.TextInput(),
             'last_name': widgets.TextInput(),
             'email': widgets.TextInput(attrs={'readonly': True}),
-            'role_system': widgets.TextInput(attrs={'readonly': True}),
+            'role_system': widgets.TextInput(attrs={'readonly': True})
         }
